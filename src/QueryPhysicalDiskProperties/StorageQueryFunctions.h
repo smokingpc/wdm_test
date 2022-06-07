69443625cba71a40e57ce3732c8d524f831a5acb
SmokingPC <smokingpc@gmail.com>
2022-06-07 14:47:58 +0800

    move app projects to "Cocktails" repository

--- src/QueryPhysicalDiskProperties/StorageQueryFunctions.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-void QueryDeviceProperties(HANDLE disk);
-void QueryAdapterProperties(HANDLE disk);
-void QueryWriteCacheProperties(HANDLE disk);
-void QueryDiskGeometry(HANDLE disk);
-void QueryDiskCapacity(HANDLE disk);
